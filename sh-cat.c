8d4eba48e90a3d2c3be97e0a57344607a5d52a98
xbz <xbz.o3sig@gmail.com>
2013-12-12 11:42:17 +0800

    add 1: sh-cat file1 ... filen. 2: sh-cat notexistfile. 3: sh-cat file1 - file2
    Signed-off-by: xbz <xbz.o3sig@gmail.com>

--- sh-cat.c
+++ sh-cat.c
@@ -1,11 +1,32 @@
 #include <stdio.h>
+#include <string.h>
+#include <errno.h>
 
 int main(int argc, char *argv[])
 {
   int c;
+  int i;
+  FILE *fp;
 
-  while ((c=getchar()) != EOF)
-    putchar(c);
+  if (argc == 1) {
+    while ((c=fgetc(stdin)) != EOF)
+      fputc(c, stdout);
+  }
+  else {
+    for (i=1; i<argc; i++) {
+      if (strcmp(argv[i], "-") == 0)
+        fp = stdin;
+      else {
+        fp = fopen(argv[i], "r");
+        if (fp == NULL) {
+          fprintf(stderr, "%s: %s: %s\n", argv[0], argv[i], strerror(errno));
+          continue;
+        }
+      }
+      while ((c=fgetc(fp)) != EOF)
+        fputc(c, stdout);
+    }
+  }
 
   return 0;
 }
